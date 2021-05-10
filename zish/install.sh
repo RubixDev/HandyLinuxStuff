9178dfd4d898172d0ad230b44b6e93ffcc1df45e
RubixDev <silas.groh@t-online.de>
2021-05-10 18:46:21 +0200

    Update install.sh

--- zish/install.sh
+++ zish/install.sh
@@ -19,23 +19,28 @@ git --version > /dev/null || {
 }
 
 # Install Oh My Zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || exit 1
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || exit 1
 }
 
 # Install necessary plugins
-git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || exit 1
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search || exit 1
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit 1
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions || exit 1
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search || exit 1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || exit 1
 
 # Install theme
-wget https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/zish/zish.zsh-theme
-mv zish.zsh-theme ~/.oh-my-zsh/custom/
+wget https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/zish/zish.zsh-theme || {
+  curl https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/zish/zish.zsh-theme || exit 1
+}
+mv zish.zsh-theme ~/.oh-my-zsh/custom/ || exit 1
 
 # Apply plugins
 perl -i -pe 's/^(plugins=\(.*?)\)/\1 zsh-autosuggestions history-substring-search zsh-syntax-highlighting)/g' ~/.zshrc || exit 1
 
 # Set theme
-perl -i -pe 's/^ZSH_THEME="zish"' ~/.zshrc
-echo 'ZSH_AUTOSUGGEST_STRATEGY=(history completion)' >> ~/.zshrc
+perl -i -pe 's/^ZSH_THEME="zish"' ~/.zshrc || exit 1
+echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >> ~/.zshrc || exit 1
+
+
+echo "Installation finished. You can set zsh as your default shell using 'chsh -s (\$which zsh)'"
 
