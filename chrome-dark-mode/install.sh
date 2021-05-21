456691057245896ea443b62aa4a223346be67eb9
RubixDev <silas.groh@t-online.de>
2021-05-21 18:04:50 +0200

    Added log messages

--- chrome-dark-mode/install.sh
+++ chrome-dark-mode/install.sh
@@ -7,10 +7,14 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Execute once now
+echo 'Applying dark mode'
 perl -i -p -e 's/(^Exec.+?stable[^-\n]*) --force-dark-mode$/\1/g' /usr/share/applications/google-chrome.desktop || exit 1
 perl -i -p -e 's/(^Exec.+?stable[^-\n]*$)/\1 --enable-features=WebUIDarkMode --force-dark-mode/g' /usr/share/applications/google-chrome.desktop || exit 1
+echo '..done'
 
 # Add to root crontab
+echo 'Adding command to crontab'
 perl -i -p -e 's/^\*\/5 \* \* \* \* perl.+?google-chrome.desktop$//g' /var/spool/cron/crontabs/root
 echo "*/5 * * * * perl -i -p -e 's/(^Exec.+?stable[^-\\n]*$)/\1 --force-dark-mode/g' /usr/share/applications/google-chrome.desktop" >> /var/spool/cron/crontabs/root || exit 1
+echo '..done'
 
