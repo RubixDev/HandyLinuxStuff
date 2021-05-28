ed30a588da8a6712edecf1a02643b53baf97bf2f
RubixDev <silas.groh@t-online.de>
2021-05-28 16:04:55 +0200

    Changed colors in FIGlet install script

--- figlet-font-installer/install.sh
+++ figlet-font-installer/install.sh
@@ -3,18 +3,18 @@
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
 
 if [ "$EUID" -ne 0 ]; then
   echo -e "${RED}Please run this script as root${NOCOLOR}"
   exit 2
 fi
 
-wget -O- https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/figlet-font-installer/fontnames.txt | while read fontname; do
-  if [ -f /usr/share/figlet/$fontname.flf ]; then
-    echo -e "${RED}Font $fontname is already installed, skipping.${NOCOLOR}"
+wget -O- https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/figlet-font-installer/fontnames.txt | while read -r fontname; do
+  if [ -f "/usr/share/figlet/$fontname.flf" ]; then
+    echo -e "${YELLOW}Font $fontname is already installed, skipping.${NOCOLOR}"
   else
-    echo -e "${YELLOW}Installing $fontname font${NOCOLOR}"
-    wget -O- http://www.figlet.org/fonts/$fontname.flf > /usr/share/figlet/$fontname.flf
+    echo -e "${CYAN}Installing $fontname font${NOCOLOR}"
+    wget -O- "http://www.figlet.org/fonts/$fontname.flf" > "/usr/share/figlet/$fontname.flf"
   fi
 done
-
