df874f8c4437a9107a0dac206d188f5efd96505f
RubixDev <silas.groh@t-online.de>
2021-06-01 20:16:02 +0200

    Rework alias scripts

--- AliasSetup/install.sh
+++ AliasSetup/install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-add_alias() { echo -e "alias $1" >> ~/.zshrc; }
+add_alias() { echo -e "alias $1='$2'" >> ~/.zshrc; }
 
-add_alias "tmux='tmux -2'"
-add_alias "zalias='vim ~/.zshrc'"
-add_alias "setclip='xclip -selection c'"
-add_alias "getclip='xclip -selection c -o'"
-add_alias "con='ssh contabo'"
-add_alias "poof='poweroff'"
-add_alias "btop='bpytop'"
-add_alias "cp='cp -iv'"
-add_alias "myip='curl ipinfo.io/ip'"
-add_alias "rr='rm -r'"
-add_alias "lelcat='bash -c \"\$(wget -O- https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/meow.sh)\"'"
-add_alias "cheat='f () { curl -s \"cheat.sh/\$1\" | less; }; f'"
+add_alias tmux 'tmux -2'
+add_alias zalias 'vim ~/.zshrc'
+add_alias setclip 'xclip -selection c'
+add_alias getclip 'xclip -selection c -o'
+add_alias lip 'getclip | zsh'
+add_alias con 'ssh contabo'
+add_alias poof poweroff
+add_alias btop bpytop
+add_alias cp 'cp -iv'
+add_alias myip 'curl ipinfo.io/ip'
+add_alias rr 'rm -r'
+# shellcheck disable=SC2016
+add_alias lelcat 'bash -c "$(wget -O- https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/meow.sh)"'
+# shellcheck disable=SC2016
+add_alias cheat 'f () { curl -s "cheat.sh/$1" | less; }; f'
+add_alias pac pacman
--- AliasSetup/removeAll.sh
+++ AliasSetup/removeAll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 perl -i -pe 's/^alias.*//g' ~/.zshrc
+perl -0777 -i -pe 's/(^\n\n)+//gm' ~/.zshrc
