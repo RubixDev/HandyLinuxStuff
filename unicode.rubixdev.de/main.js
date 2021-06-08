1d7fbd95298c3b2244a6a7afc39487c89378358c
RubixDev <silas.groh@t-online.de>
2021-06-08 17:32:11 +0200

    Move unicode website to seperate repository

--- unicode.rubixdev.de/main.js
+++
@@ -1,9 +0,0 @@
-function copyText(text) {
-    const temp = document.createElement('textarea');
-    temp.innerHTML = text;
-    document.body.appendChild(temp);
-    temp.select();
-    temp.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-    document.body.removeChild(temp)
-}
