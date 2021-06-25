1f14270daea85fca4f9b8adc7e2e9e17d6d688c5
RubixDev <silas.groh@t-online.de>
2021-06-25 17:26:20 +0200

    Change tsinit alias

--- AliasSetup/install.sh
+++ AliasSetup/install.sh
@@ -19,7 +19,7 @@ add_alias lelcat 'bash -c "$(wget -O- https://raw.githubusercontent.com/RubixDev
 add_alias cheat 'f () { curl -s "cheat.sh/$1" | less; }; f'
 add_alias pac 'sudo pacman'
 add_alias tswatch 'tsc --watch --preserveWatchOutput'
-add_alias tsinit 'wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/minify.sh && wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/tsconfig.json'
+add_alias tsinit 'wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/minify.sh && wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/tsconfig.json && chmod +x minify.sh'
 
 perl -i -pe 's/source ~\/\.config\/aliasrc//g' ~/.zshrc
 echo "source ~/.config/aliasrc" >> ~/.zshrc
