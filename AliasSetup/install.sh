37f2952149bacc8806e5c568e602506ae8092faf
RubixDev <silas.groh@t-online.de>
2021-06-28 13:17:03 +0200

    Add java aliases

--- AliasSetup/install.sh
+++ AliasSetup/install.sh
@@ -20,6 +20,9 @@ add_alias cheat 'f () { curl -s "cheat.sh/$1" | less; }; f'
 add_alias pac 'sudo pacman'
 add_alias tswatch 'tsc --watch --preserveWatchOutput'
 add_alias tsinit 'wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/minify.sh && wget https://raw.githubusercontent.com/RubixDev/HandyLinuxStuff/main/WebDev/tsconfig.json && chmod +x minify.sh'
+add_alias java8 'sudo archlinux-java set java-8-openjdk'
+add_alias java11 'sudo archlinux-java set java-11-openjdk'
+add_alias java16 'sudo archlinux-java set java-16-openjdk'
 
 perl -i -pe 's/source ~\/\.config\/aliasrc//g' ~/.zshrc
 echo "source ~/.config/aliasrc" >> ~/.zshrc
